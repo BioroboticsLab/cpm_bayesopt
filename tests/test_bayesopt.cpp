a03abe9fcd0a85b3050dd38b0b82e93d78a9255e
Benjamin Wild <b.w@fu-berlin.de>
2015-09-10 16:07:03 +0200

    add tests

---
+++ tests/test_bayesopt.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <iostream>
+
+#include <bayesopt/bayesopt.hpp>
+
+int main()
+{
+    bopt_params params = initialize_parameters_to_default();
+
+    return EXIT_SUCCESS;
+}
